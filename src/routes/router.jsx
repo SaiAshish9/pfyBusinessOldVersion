a4f5b7971058a055e18c6d6201325ddb5df76e48
amitdtu <amitdtu007@gamail.com>
2020-03-05 13:17:09 +0530

    newComp

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -9,9 +9,10 @@ import LandingPage from "../components/home/home";
 import Gig from "../components/gig/gig";
 import Internship from "../components/internship/internship";
 import PageNotFound from "../components/error/pageNotFound";
-import Marketing from '../components/marketing/marketing';
-import BoostYourInternship from '../components/boostYourInternship/boostYourInternship';
-import Help from '../components/help/help';
+import Marketing from '../components/newComp/marketing/marketing';
+import BoostYourInternship from '../components/newComp/boostYourInternship/boostYourInternship';
+import Help from '../components/newComp/help/help';
+import ReportApplicant from '../components/newComp/reportApplicant/reportApplicant';
 
 const history = createBrowserHistory();
 
@@ -24,7 +25,7 @@ export default function AppRouter() {
         <Route path="/internship" component={Internship} exact={true} />
         <Route path="/gig" component={Gig} exact={true} />
         <Route path="/marketing" component={Marketing} exact={true} />
-        <Route path="/help" component={BoostYourInternship} exact={true} />
+        <Route path="/testing" component={ReportApplicant} exact={true} />
         <Route component={PageNotFound} />
       </Switch>
     </Router>
