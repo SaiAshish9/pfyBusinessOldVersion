ae238eebce7d00791352f0e74d41d9f9ba5c9592
mourrrya <mourrrya@gmail.com>
2020-04-19 19:30:34 +0530

    create home page

--- continous.sh
+++ continous.sh
@@ -5,7 +5,7 @@ git commit -m "$1"
 git push origin master
 
 
-ssh -i /home/roopam/.ssh/pracify_keys ubuntu@35.154.0.19 << 'ENDSSH'
+ssh -i 'C:\Users\Administrator\Downloads\hello.pem' ubuntu@35.154.0.19 << 'ENDSSH'
 cd ./pracify-business-ui
 git add .
 git commit -m "server changes"
