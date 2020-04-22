d6222bae553a6303e8451d84d6039b2b823c566d
mourrrya <mourrrya@gmail.com>
2020-04-22 16:50:45 +0530

    create upload logo at signup

--- src/components/loginOrSignUp/signUp.jsx
+++ src/components/loginOrSignUp/signUp.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Input, Button, Select } from "antd";
+import React, { useState, useRef } from "react";
+import { Input, Button, Select, Upload, message } from "antd";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
 import { useHistory } from "react-router-dom";
 import { useForm, Controller } from "react-hook-form";
 import cookie from "js-cookie";
@@ -35,9 +36,28 @@ const companyDetail = [
 const { Option } = Select;
 const { TextArea } = Input;
 
+const getBase64 = (img, callback) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
+
 export default function SignUp() {
   const history = useHistory();
-  const { control, handleSubmit, watch, reset } = useForm({
+  const inputRef = useRef();
+  const { control, handleSubmit, watch, reset, errors } = useForm({
     defaultValues: {},
   });
 
@@ -67,6 +87,8 @@ export default function SignUp() {
 
   const [isRegister, setIsRegister] = useState(false);
   const [isFinalRegister, setIsFinalRegister] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
 
   const whichInputField = (inputType, formName) => {
     switch (inputType) {
@@ -75,8 +97,14 @@ export default function SignUp() {
       }
       case "select": {
         return (
-          <Select className={`${formName}__input`}>
-            {/* <Option>hello</Option> */}
+          <Select
+            className={`${formName}__input`}
+            listItemHeight={10}
+            listHeight={250}
+          >
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((data, index) => (
+              <Option key={index}> {data}</Option>
+            ))}
           </Select>
         );
       }
@@ -88,6 +116,21 @@ export default function SignUp() {
       }
     }
   };
+
+  const handleUpload = (info) => {
+    if (info.file.status === "uploading") {
+      setLoading({ loading: true });
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (imageUrl) => {
+        setLoading(false);
+        setImageUrl(imageUrl);
+      });
+    }
+  };
+
   return (
     <div className="login-main-block">
       <div className="login-form-block">
@@ -130,38 +173,67 @@ export default function SignUp() {
               <p className="email-otp__label">Email OTP</p>
               <Controller
                 as={<Input className="email-otp__input" />}
-                name={`${userDetail.formName}`}
+                name="otp"
                 control={control}
+                rules={{ required: true, minLength: 3 }}
               />
+              {errors.otp && errors.otp.type === "required" && (
+                <span>This is required</span>
+              )}
+              {errors.otp && errors.otp.type === "minLength" && (
+                <span>should have 3 character</span>
+              )}
             </div>
             <Button htmlType="submit" className="register__button">
               VERIFY OTP
             </Button>
           </form>
         ) : (
-          <form
-            onSubmit={handleSubmit(onSubmitFinalRegister)}
-            className="signUp-form-block"
-          >
-            {companyDetail.map((companyDetail, index) => (
-              <div className={`${companyDetail.formName}`} key={index}>
-                <p className={`${companyDetail.formName}__label`}>
-                  {companyDetail.name}
-                </p>
-                <Controller
-                  as={whichInputField(
-                    companyDetail.type,
-                    companyDetail.formName
-                  )}
-                  name={`${companyDetail.formName}`}
-                  control={control}
-                />
-              </div>
-            ))}
-            <Button htmlType="submit" className="register__button">
-              REGISTER
-            </Button>
-          </form>
+          <>
+            <form
+              onSubmit={handleSubmit(onSubmitFinalRegister)}
+              className="signUp-form-block"
+            >
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                beforeUpload={beforeUpload}
+                onChange={handleUpload}
+              >
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                ) : (
+                  <div>
+                    {loading ? <LoadingOutlined /> : <PlusOutlined />}
+                    <div className="ant-upload-text">Upload</div>
+                  </div>
+                )}
+              </Upload>
+
+              {companyDetail.map((companyDetail, index) => (
+                <div className={`${companyDetail.formName}`} key={index}>
+                  <p className={`${companyDetail.formName}__label`}>
+                    {companyDetail.name}
+                  </p>
+                  <Controller
+                    as={whichInputField(
+                      companyDetail.type,
+                      companyDetail.formName
+                    )}
+                    name={`${companyDetail.formName}`}
+                    control={control}
+                    // onFocus={() => inputRef.current.focus()}
+                  />
+                </div>
+              ))}
+              <Button htmlType="submit" className="register__button">
+                REGISTER
+              </Button>
+            </form>
+          </>
         )}
 
         {!isRegister && (
