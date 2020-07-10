fc2906dc5f18a79aba8bb8b9c6a40e3bede26819
SaiAshish9 <saiashish3760@gmail.com>
2020-07-10 20:26:10 +0530

    imported aos and animate.css in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "antd/dist/antd.css";
-import React from "react";
+import React, { useEffect } from "react";
 import AppRouter from "./routes/router.jsx";
+
+import "aos/dist/aos.css";
+import "animate.css/animate.min.css";
+import Aos from "aos";
 
 import "./styles/_style.scss";
 
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 400 });
+  }, []);
+
   return <AppRouter></AppRouter>;
 }
 
